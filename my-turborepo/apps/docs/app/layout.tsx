67847c6758647a7c06fd3d6f8b2dc06374fea7e5
Turbobot <turbobot@vercel.com>
2024-10-10 03:11:20 +0200

    feat(create-turbo): apply official-starter transform

--- my-turborepo/apps/docs/app/layout.tsx
+++ my-turborepo/apps/docs/app/layout.tsx
@@ -5,12 +5,10 @@ import "./globals.css";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
